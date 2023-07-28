72e3d0d90caf9c15c9b9734f2912659572e4a236
ZaField <zakyhafiedz10@gmail.com>
2023-07-28 16:49:44 +0700

    testing feature

---
+++ assets/js/swiper.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper ('#section-wrapper', {
+    // If we need pagination
+    direction: "vertical",
+    slidesPerView: "auto",
+    mousewheel: true,
+    keyboard: true,
+    speed: 1500,
+    on: {
+        slideChange: function () {
+            const activeIndex = swiper.activeIndex;
+            const activeItem = document.querySelector('.menu-list li span.active');
+            if (activeItem) {
+              activeItem.classList.remove('active');
+            }
+            const menuItems = document.querySelectorAll('.menu-list li span');
+            menuItems[activeIndex].classList.add('active');
+        }
+    }
+  })
+
+
+const icon = document.querySelector('.navigation-wrapper svg')
+const menu_list = document.querySelector('.menu-list')
+
+icon.addEventListener('click', function() {
+    menu_list.classList.toggle('slide');
+});
+
+const menu_items = document.querySelectorAll('.menu-list li span a');
+menu_items.forEach((item, index)=> {
+  item.addEventListener('click', function() {
+    const activeItem = document.querySelector('.menu-list li span.active');
+    if (activeItem) {
+      activeItem.classList.remove('active');
+    }
+    item.parentNode.classList.add('active');
+    swiper.slideTo(index);
+  });
+});
+
+const features_swiper_container = document.querySelector('#features-swiper');
+const features_wrapper = features_swiper_container.querySelector('.swiper-wrapper');
+const features_slide = features_wrapper.querySelectorAll('.swiper-slide');
